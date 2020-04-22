6ce5d8a8c9a0e7b40c0215d58e4b57e2c264adb2
kovila77 <354981@mail.ru>
2020-04-22 19:39:45 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ORM_Resourses
+{
+    partial class fRConsume
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tpRes = new System.Windows.Forms.TabPage();
+            this.tpResConsume = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.правкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl.SuspendLayout();
+            this.tpRes.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tpRes);
+            this.tabControl.Controls.Add(this.tpResConsume);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 24);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(668, 243);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tpRes
+            // 
+            this.tpRes.Controls.Add(this.dataGridView1);
+            this.tpRes.Location = new System.Drawing.Point(4, 22);
+            this.tpRes.Name = "tpRes";
+            this.tpRes.Padding = new System.Windows.Forms.Padding(3);
+            this.tpRes.Size = new System.Drawing.Size(660, 217);
+            this.tpRes.TabIndex = 0;
+            this.tpRes.Text = "Ресурсы";
+            this.tpRes.UseVisualStyleBackColor = true;
+            // 
+            // tpResConsume
+            // 
+            this.tpResConsume.Location = new System.Drawing.Point(4, 22);
+            this.tpResConsume.Name = "tpResConsume";
+            this.tpResConsume.Padding = new System.Windows.Forms.Padding(3);
+            this.tpResConsume.Size = new System.Drawing.Size(660, 217);
+            this.tpResConsume.TabIndex = 1;
+            this.tpResConsume.Text = "Потребление ресурсов";
+            this.tpResConsume.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.правкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(668, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // правкаToolStripMenuItem
+            // 
+            this.правкаToolStripMenuItem.Name = "правкаToolStripMenuItem";
+            this.правкаToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.правкаToolStripMenuItem.Text = "Правка";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(654, 211);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // fRConsume
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 267);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "fRConsume";
+            this.Text = "Потребление ресурсов";
+            this.tabControl.ResumeLayout(false);
+            this.tpRes.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tpRes;
+        private System.Windows.Forms.TabPage tpResConsume;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem правкаToolStripMenuItem;
+    }
+}
+
